d021e4ec76679e9186c3f8042883ea3285fc1549
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-10 23:23:04 -0400

    add functions

--- W4D1/graphnode.rb
+++ W4D1/graphnode.rb
@@ -1,11 +1,19 @@
 class GraphNode
 
     attr_accessor :neighbors
+
+    def self.bfs(starting_node, target_value)
+        explored = [starting_node]
+        return starting_node if starting_node.value == target_value
+        
+    end
+
     def initialize(value)
         @value = value
         @neighbors = []
     end
 
+    
     
 end
 a = GraphNode.new('a')
