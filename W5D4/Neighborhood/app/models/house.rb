7893cd761192f7e849c56e29c0c879741b96b75e
Donnie Wombough <donniewomboughjunior@yahoo.com>
2021-05-20 09:12:42 -0400

    add W5D4 homework

---
+++ W5D4/Neighborhood/app/models/house.rb
@@ -0,0 +1,10 @@
+class House < ApplicationRecord
+    validates :address, presence: true
+    # validate(:check_name_length)
+
+    # def check_name_length
+    #     unless self.name.length >= 4
+    #         errors[:name] << "is too short; must be longer than 3 characters."
+    #     end
+    # end
+end
---
+++ W5D4/Neighborhood/app/models/person.rb
@@ -0,0 +1,11 @@
+class Person < ApplicationRecord
+    validates :name, presence: true
+    validates :house_id, presence: true
+    # validate(:check_name_length)
+
+    # def check_name_length
+    #     unless self.name.length >= 4
+    #         errors[:name] << "is too short; must be longer than 3 characters."
+    #     end
+    # end
+end
